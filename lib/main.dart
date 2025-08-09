bbf766f83f41865b841777f20b8e01c52872b11f
Chorakit <jorakit.work@gmail.com>
2025-08-09 10:44:35 +0700

    ีupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_budget/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
-  runApp(MaterialApp(theme: ThemeData(useMaterial3: true), home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+      ).copyWith(
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.all(4),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: Expenses(),
+    ),
+  );
 }
